1b38116732d972d7d5630fc73e8437bcaa874cad
Junsung Choi <junsungc91@gmail.com>
2023-05-05 22:53:35 +0900

    Bus info 시각화, 과거 Marker & Info 기록 삭제

--- src/Map.js
+++ src/Map.js
@@ -42,152 +42,161 @@ const Map = () => {
                 var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
                 message += '경도는 ' + latlng.getLng() + ' 입니다';
             });
-
-
-            let res = [];
-            let url = 'http://apis.data.go.kr/1613000/BusLcInfoInqireService/getRouteAcctoBusLcList'; /*URL*/
-            let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D'; /*Service Key*/
-            queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
-            queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('xml'); /**/
-            queryParams += '&' + encodeURIComponent('cityCode') + '=' + encodeURIComponent('31240'); /**/
-            queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('GGB233000266'); /**/
-            try {
-                const response = await sendHttpRequest('GET', url, queryParams);
-                if (response.status === 200) {
-                    var xmlDoc = response.responseXML;
-                    var items = xmlDoc.getElementsByTagName('item');
-                    // 파싱된 데이터 사용
-                    for (var i = 0; i < items.length; i++) {
-                        var latitude = items[i].getElementsByTagName('gpslati')[0].childNodes[0].nodeValue;
-                        var longitude = items[i].getElementsByTagName('gpslong')[0].childNodes[0].nodeValue;
-                        var vehicleno = items[i].getElementsByTagName('vehicleno')[0].childNodes[0].nodeValue;
-                        res.push({ latitude, longitude, vehicleno })
-                    }
-                } else {
-                    console.error("status 에러 발생:", response.statusText);
-                }
-            } catch (error) {
-                console.error("에러 발생:", error);
-            }
-
-            const busStation = [];
-            url = 'http://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteAcctoThrghSttnList';
-            queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D';
-            queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-            queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
-            queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('xml'); /**/
-            queryParams += '&' + encodeURIComponent('cityCode') + '=' + encodeURIComponent('31240'); /**/
-            queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('GGB233000266'); /**/
-            try {
-                const response = await sendHttpRequest('GET', url, queryParams);
-                if (response.status === 200) {
-                    var xmlDoc = response.responseXML;
-                    var items = xmlDoc.getElementsByTagName('item');
-                    // 파싱된 데이터 사용
-                    for (var i = 0; i < items.length; i++) {
-                        var latitude = items[i].getElementsByTagName('gpslati')[0].childNodes[0].nodeValue;
-                        var longitude = items[i].getElementsByTagName('gpslong')[0].childNodes[0].nodeValue;
-                        var nodeord = items[i].getElementsByTagName('nodeord')[0].childNodes[0].nodeValue;
-                        busStation.push({ latitude, longitude, nodeord })
-                    }
-                } else {
-                    console.error("status 에러 발생:", response.statusText);
-                }
-            } catch (error) {
-                console.error("에러 발생:", error);
-            }
-
-            url = 'http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList';
-            queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D';
-            queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('233000266');
-            try {
-                const response = await sendHttpRequest('GET', url, queryParams);
-                if (response.status === 200) {
-                    let extraData = [];
-                    var xmlDoc = response.responseXML;
-                    var items = xmlDoc.getElementsByTagName('busLocationList');
-                    // 파싱된 데이터 사용
-                    for (var i = 0; i < items.length; i++) {
-                        let remainSeatCnt = items[i].getElementsByTagName('remainSeatCnt')[0].childNodes[0].nodeValue;
-                        let stationSeq = items[i].getElementsByTagName('stationSeq')[0].childNodes[0].nodeValue;
-                        let plateNo = items[i].getElementsByTagName('plateNo')[0].childNodes[0].nodeValue;
-                        let added = false;
-                        res.forEach((n, i) => {
-                            if (n.vehicleno === plateNo) {
-                                res[i].remainSeatCnt = remainSeatCnt;
-                                res[i].stationSeq = stationSeq;
-                                added = true;
-                            }
-                        });
-                        if (!added) {
-                            busStation.forEach((n, i) => {
-                                if (n.nodeord === stationSeq) {
-                                    extraData.push({
-                                        remainSeatCnt,
-                                        stationSeq,
-                                        vehicleno: plateNo,
-                                        latitude: n.latitude,
-                                        longitude: n.longitude
-                                    })
-                                }
-                            })
-                        }
-                    }
-                    res = [...res, ...extraData]
-                    dispatch({
-                        type: 'SETBUSLOCATION',
-                        busLocation: res
-                    });
-                } else {
-                    console.error("에러 발생:", response.statusText);
-                }
-            } catch (error) {
-                console.error("에러 발생:", error);
-            }
+            setBusLocation()
         })();
     }, []);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
+            setBusLocation()
             setBusMarker()
         }, 5000);
 
         return () => clearInterval(intervalId);
     }, [busLocation]);
 
+    const setBusLocation = async () => {
+        let res = [];
+        let url = 'http://apis.data.go.kr/1613000/BusLcInfoInqireService/getRouteAcctoBusLcList'; /*URL*/
+        let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D'; /*Service Key*/
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
+        queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('xml'); /**/
+        queryParams += '&' + encodeURIComponent('cityCode') + '=' + encodeURIComponent('31240'); /**/
+        queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('GGB233000266'); /**/
+        try {
+            const response = await sendHttpRequest('GET', url, queryParams);
+            if (response.status === 200) {
+                var xmlDoc = response.responseXML;
+                var items = xmlDoc.getElementsByTagName('item');
+                // 파싱된 데이터 사용
+                for (var i = 0; i < items.length; i++) {
+                    var latitude = items[i].getElementsByTagName('gpslati')[0].childNodes[0].nodeValue;
+                    var longitude = items[i].getElementsByTagName('gpslong')[0].childNodes[0].nodeValue;
+                    var vehicleno = items[i].getElementsByTagName('vehicleno')[0].childNodes[0].nodeValue;
+                    res.push({ latitude, longitude, vehicleno })
+                }
+            } else {
+                console.error("status 에러 발생:", response.statusText);
+            }
+        } catch (error) {
+            console.error("에러 발생:", error);
+        }
+
+        const busStation = [];
+        url = 'http://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteAcctoThrghSttnList';
+        queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D';
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+        queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /**/
+        queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('xml'); /**/
+        queryParams += '&' + encodeURIComponent('cityCode') + '=' + encodeURIComponent('31240'); /**/
+        queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('GGB233000266'); /**/
+        try {
+            const response = await sendHttpRequest('GET', url, queryParams);
+            if (response.status === 200) {
+                var xmlDoc = response.responseXML;
+                var items = xmlDoc.getElementsByTagName('item');
+                // 파싱된 데이터 사용
+                for (var i = 0; i < items.length; i++) {
+                    var latitude = items[i].getElementsByTagName('gpslati')[0].childNodes[0].nodeValue;
+                    var longitude = items[i].getElementsByTagName('gpslong')[0].childNodes[0].nodeValue;
+                    var nodeord = items[i].getElementsByTagName('nodeord')[0].childNodes[0].nodeValue;
+                    busStation.push({ latitude, longitude, nodeord })
+                }
+            } else {
+                console.error("status 에러 발생:", response.statusText);
+            }
+        } catch (error) {
+            console.error("에러 발생:", error);
+        }
+
+        url = 'http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList';
+        queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D';
+        queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('233000266');
+        try {
+            const response = await sendHttpRequest('GET', url, queryParams);
+            if (response.status === 200) {
+                let extraData = [];
+                var xmlDoc = response.responseXML;
+                var items = xmlDoc.getElementsByTagName('busLocationList');
+                // 파싱된 데이터 사용
+                for (var i = 0; i < items.length; i++) {
+                    let remainSeatCnt = items[i].getElementsByTagName('remainSeatCnt')[0].childNodes[0].nodeValue;
+                    let stationSeq = items[i].getElementsByTagName('stationSeq')[0].childNodes[0].nodeValue;
+                    let plateNo = items[i].getElementsByTagName('plateNo')[0].childNodes[0].nodeValue;
+                    let added = false;
+                    res.forEach((n, i) => {
+                        if (n.vehicleno === plateNo) {
+                            res[i].remainSeatCnt = remainSeatCnt;
+                            res[i].stationSeq = stationSeq;
+                            added = true;
+                        }
+                    });
+                    if (!added) {
+                        busStation.forEach((n, i) => {
+                            if (n.nodeord === stationSeq) {
+                                extraData.push({
+                                    remainSeatCnt,
+                                    stationSeq,
+                                    vehicleno: plateNo,
+                                    latitude: n.latitude,
+                                    longitude: n.longitude
+                                })
+                            }
+                        })
+                    }
+                }
+                res = [...res, ...extraData]
+                dispatch({
+                    type: 'SETBUSLOCATION',
+                    busLocation: res
+                });
+            } else {
+                console.error("에러 발생:", response.statusText);
+            }
+        } catch (error) {
+            console.error("에러 발생:", error);
+        }
+    }
     const setBusMarker = () => {
-        console.log('setBusMaker')
         if (busLocation) {
-            console.log(busLocation)
+            var markers = [];
+            var infoWindows = [];
             busLocation.forEach(e => {
                 // 마커가 표시될 위치입니다 
                 var markerPosition = new kakao.maps.LatLng(e.latitude, e.longitude);
                 // 마커를 생성합니다
                 var marker = new kakao.maps.Marker({
                     position: markerPosition
-                });
-                marker.setMap(currentMap);
-                var iwContent = `<div style="padding:5px;">${e.stationSeq}정류소 ${e.remainSeatCnt}석</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-                    iwPosition = new kakao.maps.LatLng(e.latitude, e.longitude); //인포윈도우 표시 위치입니다
+                })
+                var iwContent = `<div class ="label"><span class="left"></span><span class="center">${e.stationSeq}정류소 ${e.remainSeatCnt}석</span><span class="right"></span></div>` // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+                var iwPosition = new kakao.maps.LatLng(e.latitude, e.longitude); //인포윈도우 표시 위치입니다
 
                 // 인포윈도우를 생성합니다
-                var infowindow = new kakao.maps.InfoWindow({
+                var infowindow = new kakao.maps.CustomOverlay({
                     position: iwPosition,
                     content: iwContent
                 });
-
-
-                kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(currentMap, marker, infowindow));
+                markers.push(marker);
+                infoWindows.push(infowindow);
             });
+            showMarker(currentMap, markers, infoWindows);
         }
     }
-    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-    const makeOverListener = (map, marker, infowindow) => {
-        return function () {
-            infowindow.open(map, marker);
-        };
+
+    const showMarker = (map, markers, infoWindows) => {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            infoWindows[i].setMap(map);
+            setTimeout((markers, infoWindows) => {
+                for (var i = 0; i < markers.length; i++) {
+                    markers[i].setMap(null);
+                    infoWindows[i].setMap(null);
+                }
+            }, 5000, markers, infoWindows);
+        }
     }
+
     const sendHttpRequest = (method, url, param) => {
         return new Promise(function (resolve, reject) {
             var xhr = new XMLHttpRequest();
@@ -205,7 +214,6 @@ const Map = () => {
             xhr.send('');
         });
     }
-
 
     const getUserLocation = async () => {
         const getLocation = () => {
@@ -243,6 +251,7 @@ const Map = () => {
                 }}>
             </div>
             <hr />
+            <button onClick={() => showMarker(null)}>클릭</button>
         </>
     )
 }
--- src/component/Bus.js
+++ src/component/Bus.js
@@ -1,54 +1,11 @@
-import React, { useEffect, useState, useRef } from "react"
+import React, { useEffect, } from "react"
 import { useSelector, useDispatch } from 'react-redux';
-
-const { kakao } = window
 
 const Bus = () => {
     const dispatch = useDispatch();
     const busLocation = useSelector(state => state.busLocation);
 
     useEffect(() => {
-
-        // 도시 코드 조회 //
-        // let url = 'http://apis.data.go.kr/1613000/BusLcInfoInqireService/getCtyCodeList'; /*URL*/
-        // let queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D'; /*Service Key*/
-        // queryParams += '&' + encodeURIComponent('_type') + '=' + encodeURIComponent('xml'); /**/
-        // xhr.open('GET', url + queryParams);
-        // xhr.onreadystatechange = function () {
-        //     if (this.readyState === 4 && this.status === 200) {
-
-        //         var xmlDoc = this.responseXML;
-        //         var items = xmlDoc.getElementsByTagName('item');
-
-        //         // 파싱된 데이터 사용
-        //         for (var i = 0; i < items.length; i++) {
-        //             var citycode = items[i].getElementsByTagName('citycode')[0].childNodes[0].nodeValue;
-        //             var cityname = items[i].getElementsByTagName('cityname')[0].childNodes[0].nodeValue;
-        //         }
-        //         //화성시 31240
-        //     }
-        // };
-
-        // 도시별 버스노선 조회 //
-        // url = 'http://apis.data.go.kr/1613000/BusRouteInfoInqireService/getRouteNoList'; /*URL*/
-        // queryParams = '?' + encodeURIComponent('serviceKey') + '=' + 'UfIEM6jLupWmJHj4FCZPTi7m%2BFS1n8bIzTCxU7CF4vc%2FmiustktE9a1kcd37Gxnq7m5aVKqoBONQzgEPw97Nwg%3D%3D'; /*Service Key*/
-        // queryParams += '&' + encodeURIComponent('cityCode') + '=' + encodeURIComponent('31240'); /**/
-        // queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000'); /**/
-        // xhr.open('GET', url + queryParams);
-        // xhr.onreadystatechange = function () {
-        //     if (this.readyState == 4 && this.status === 200) {
-        //         var xmlDoc = this.responseXML;
-        //         var items = xmlDoc.getElementsByTagName('item');
-
-        //         // 파싱된 데이터 사용
-        //         for (var i = 0; i < items.length; i++) {
-        //             var citycode = items[i].getElementsByTagName('routeid')[0].childNodes[0].nodeValue;
-        //             var cityname = items[i].getElementsByTagName('routeno')[0].childNodes[0].nodeValue;
-        //         }
-        //         //6003 GGB233000266 
-        //     }
-        // };
-        console.log('useEffect');
         (async () => {
             let xhr = new XMLHttpRequest();
             let url = 'http://apis.data.go.kr/1613000/BusLcInfoInqireService/getRouteAcctoBusLcList'; /*URL*/
@@ -61,7 +18,7 @@ const Bus = () => {
             xhr.open('GET', url + queryParams);
             let res = []
             xhr.onreadystatechange = await function () {
-                if (this.readyState == 4 && this.status === 200) {
+                if (this.readyState === 4 && this.status === 200) {
                     var xmlDoc = this.responseXML;
                     var items = xmlDoc.getElementsByTagName('item');
                     // 파싱된 데이터 사용
@@ -83,7 +40,7 @@ const Bus = () => {
             queryParams += '&' + encodeURIComponent('routeId') + '=' + encodeURIComponent('233000266');
             xhr.open('GET', url + queryParams);
             xhr.onreadystatechange = await function () {
-                if (this.readyState == 4 && this.status === 200) {
+                if (this.readyState === 4 && this.status === 200) {
                     var xmlDoc = this.responseXML;
                     var items = xmlDoc.getElementsByTagName('busLocationList');
                     // 파싱된 데이터 사용
@@ -93,7 +50,6 @@ const Bus = () => {
                         res.forEach((n, i) => {
                             if (n.vehicleno === plateNo) {
                                 res[i].remainSeatCnt = remainSeatCnt
-                                console.log('hii')
                             }
                         });
                     }
